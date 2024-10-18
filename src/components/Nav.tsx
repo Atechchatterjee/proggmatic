795040728040d5aa153015e8147684248cdacd18
Atechchatterjee <atechchatterjeee@gmail.com>
2024-10-18 22:55:41 +0530

    feat: add a basic navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,20 +1,30 @@
 import { useLocation } from "@solidjs/router";
 
+function LeftNav() {
+  return <div class="border-r-[1px] border-r-white border-dashed w-[15rem]"></div>
+}
+
+function RightNav() {
+  return <div class="border-l-[1px] border-l-white border-dashed w-[15rem]"></div>
+}
+
 export default function Nav() {
   const location = useLocation();
-  const active = (path: string) =>
-    path == location.pathname ? "border-sky-600" : "border-transparent hover:border-sky-600";
+
+  console.log("location = ", location);
 
   return (
-    <nav class="bg-sky-800">
-      <ul class="container flex items-center p-3 text-gray-200">
-        <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`}>
-          <a href="/">Home</a>
-        </li>
-        <li class={`border-b-2 ${active("/about")} mx-1.5 sm:mx-6`}>
-          <a href="/about">About</a>
-        </li>
-      </ul>
+    <nav class="flex px-8 w-[100%] border-b-[1px] border-b-white">
+      <LeftNav />
+      <div class="flex w-full items-center px-8 py-5 ">
+        <img src="/logo-white.svg" width={50} height={50} />
+        <div class="flex ml-auto gap-8">
+          <a>Home</a>
+          <a>Challenges</a>
+          <a>Login</a>
+        </div>
+      </div>
+      <RightNav />
     </nav>
   );
 }
